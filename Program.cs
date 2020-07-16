3d3e96108ff1833e1de8d80163aadfd6857a0717
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-07-16 16:32:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab_3._3_Data_Structures
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Console.Write($"Please enter a word you would like to reverse: ");
+            string entry  = Console.ReadLine();
+            
+            Console.WriteLine($"Your word in reverse is: {reverse(entry)}");
+        }
+
+        static string reverse(string word)
+        {
+
+            Stack<char> myStack = new Stack<char>();
+            
+            for(int i =0 ; i < word.Length; i++)
+            {
+                myStack.Push(word[i]);
+            }
+
+            string reverseWord = null;
+
+            while (myStack.Count != 0)
+            {
+                reverseWord += myStack.Pop();
+            }
+
+            return reverseWord;
+
+        }
+                        
+    }
+    
+}
